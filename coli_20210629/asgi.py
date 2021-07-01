c66f208ad358efe70d46072bbcccafd060c322b8
als90 <als90915@gmail.com>
2021-07-01 11:35:50 +0900

    7/1 web/app class commit

---
+++ coli_20210629/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for coli_20210629 project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coli_20210629.settings')
+
+application = get_asgi_application()
---
+++ coli_20210629/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for coli_20210629 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coli_20210629.settings')
+
+application = get_wsgi_application()
