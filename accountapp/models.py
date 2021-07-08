58785255af9811b5007a5cbcc9cdb74c6babcd29
als90 <als90915@gmail.com>
2021-07-08 11:53:03 +0900

    7/8 commit_2

--- accountapp/models.py
+++ accountapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+
 # Create your models here.
+
+class HelloWorld(models.Model):
+    text = models.CharField(max_length=255, null=False)  # 텍스트 최대길이 255, null 값이 있으면 안됨
