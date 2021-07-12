2579fec9d84cae83331f37e07baeee6a6b0736e2
als90 <als90915@gmail.com>
2021-07-12 11:27:34 +0900

    7/12 commit_2

--- accountapp/views.py
+++ accountapp/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 
 
 # Create your views here.
+from accountapp.models import HelloWorld
 
 
 def hello_world(request):
@@ -10,8 +11,14 @@ def hello_world(request):
 
         temp = request.POST.get('input_text')
 
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+        # 객체를 DB에 저장    # temp와 다르게 DB에 저장됨(왼쪽 파일 목록 db.sqlite3)
+        # settings.py의 DATABASE 내용 확인 해보면 지정된걸 확인할수있음
+
         return render(request, 'accountapp/hello_world.html',
-                      context={'text': temp})
+                      context={'new_hello_world': new_hello_world})
     else:
         return render(request, 'accountapp/hello_world.html',
                       context={'text': 'GET METHOD!'})
