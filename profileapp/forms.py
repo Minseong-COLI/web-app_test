fa16b14948454a1896656d582d4aeec4dd63a6a2
als90 <als90915@gmail.com>
2021-07-26 11:52:16 +0900

    7/26 commit_2

---
+++ profileapp/forms.py
@@ -0,0 +1,11 @@
+from django.forms import ModelForm
+
+from profileapp.models import Profile
+
+
+class ProfileCreationForm(ModelForm):
+    # 장고에서 제공하는 ModelForm 을 상속 받아옴
+    class Meta:
+        model = Profile
+        fields = ['image', 'nickname', 'message']
+        # user는 target_user를 고려해야 하기 때문에 다른곳에서 처리?
--- profileapp/models.py
+++ profileapp/models.py
@@ -8,8 +8,8 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     # 장고에서 제공하는 User를 객채로 받아옴
     # on_delete: 삭제 됐을때 실행 됨
-        # CASCADE: 연결되어있는(종속되어있는) 것도 삭제 -> 게시글 등 관련된 것 모두 삭제
-        # SET_NULL: 삭제 되었을때 User를 Null로 바꿈
+    #     CASCADE: 연결되어있는(종속되어있는) 것도 삭제 -> 게시글 등 관련된 것 모두 삭제
+    #     SET_NULL: 삭제 되었을때 User를 Null로 바꿈 -> 게시글에 User가 Null로 표기됨
     # related_name: 접근하는 방식(연결고리)  ex)target_user.profile
     image = models.ImageField(upload_to='profile/', null=True)
     # upload_to: 사진의 저장 경로
