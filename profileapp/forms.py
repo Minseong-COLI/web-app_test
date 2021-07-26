c36d80f081ab023c5826bc44c680488ef7a69bee
als90 <als90915@gmail.com>
2021-07-26 12:20:48 +0900

    7/26 commit_3

--- profileapp/forms.py
+++ profileapp/forms.py
@@ -8,4 +8,5 @@ class ProfileCreationForm(ModelForm):
     class Meta:
         model = Profile
         fields = ['image', 'nickname', 'message']
+        # field 순서대로 입력 창이 나온다
         # user는 target_user를 고려해야 하기 때문에 다른곳에서 처리?
--- profileapp/urls.py
+++ profileapp/urls.py
@@ -1,5 +1,11 @@
+from django.urls import path
+
+from profileapp.views import ProfileCreateView
+
+app_name = 'profileapp'
+# profileapp 폴더의 라우트 경로 지정
 
 
 urlpatterns = [
-
+    path('create/', ProfileCreateView.as_view(), name='create')
 ]
