0fd422614be4793c5cba26c3b5b6efa0741249e4
als90 <als90915@gmail.com>
2021-07-27 18:04:13 +0900

    7/27 commit_4

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from profileapp.views import ProfileCreateView
+from profileapp.views import ProfileCreateView, ProfileUpdateView
 
 app_name = 'profileapp'
 # profileapp 폴더의 라우트 경로 지정
 
 
 urlpatterns = [
-    path('create/', ProfileCreateView.as_view(), name='create')
+    path('create/', ProfileCreateView.as_view(), name='create'),
+    path('update/<int:pk>', ProfileUpdateView.as_view(), name='update')
 ]
--- profileapp/views.py
+++ profileapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 
 from profileapp.forms import ProfileCreationForm
 from profileapp.models import Profile
@@ -19,7 +19,13 @@ class ProfileCreateView(CreateView):
         form.instance.user = self.request.user
         # form이 들고있는 검증된 데이터를 instance(profile)에 넣어줌?
         return super().form_valid(form)
-
-
     # def form_invalid(self, form):
         # 위의 검증 중 하나가 실패했을때 실행되는 함수
+
+
+class ProfileUpdateView(UpdateView):
+    model = Profile
+    context_object_name = 'target_profile'
+    form_class = ProfileCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'profileapp/update.html'
