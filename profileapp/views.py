806d0345f9570ac4707069073bf5aba187e959c9
als90 <als90915@gmail.com>
2021-07-27 16:51:26 +0900

    7/27 commit_1

--- profileapp/views.py
+++ profileapp/views.py
@@ -13,3 +13,13 @@ class ProfileCreateView(CreateView):
     form_class = ProfileCreationForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/create.html'
+
+    def form_valid(self, form):
+        # 위의 검증이 모두 성공했을때 실행되는 함수
+        form.instance.user = self.request.user
+        # form이 들고있는 검증된 데이터를 instance(profile)에 넣어줌?
+        return super().form_valid(form)
+
+
+    # def form_invalid(self, form):
+        # 위의 검증 중 하나가 실패했을때 실행되는 함수
