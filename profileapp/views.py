4f7926c3080528e3ec3ecb7e8f132cc82df10286
als90 <als90915@gmail.com>
2021-07-29 11:21:27 +0900

    7/29 commit_1

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,13 +1,18 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, UpdateView
 
+from profileapp.decorators import profile_ownership_required
 from profileapp.forms import ProfileCreationForm
 from profileapp.models import Profile
 
 
+@method_decorator(login_required, 'get')
+@method_decorator(login_required, 'post')
 class ProfileCreateView(CreateView):
     model = Profile
     form_class = ProfileCreationForm
@@ -18,11 +23,14 @@ class ProfileCreateView(CreateView):
         # 위의 검증이 모두 성공했을때 실행되는 함수
         form.instance.user = self.request.user
         # form이 들고있는 검증된 데이터를 instance(profile)에 넣어줌?
+        # user를 인식하기 위한 작업
         return super().form_valid(form)
     # def form_invalid(self, form):
         # 위의 검증 중 하나가 실패했을때 실행되는 함수
 
 
+@method_decorator(profile_ownership_required, 'get')
+@method_decorator(profile_ownership_required, 'post')
 class ProfileUpdateView(UpdateView):
     model = Profile
     context_object_name = 'target_profile'
