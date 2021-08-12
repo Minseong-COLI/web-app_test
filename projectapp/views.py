e722964bc13b887d1bba5b22c17dd360bcf19894
als90 <als90915@gmail.com>
2021-08-12 12:35:54 +0900

    8/12 commit_6

--- projectapp/views.py
+++ projectapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 
 from projectapp.forms import ProjectCreationForm
 from projectapp.models import Project
@@ -14,3 +14,10 @@ class ProjectCreateView(CreateView):
     success_url = reverse_lazy('articleapp:list')
     # article list로 임시 지정 -> 이후 detailview 생성후 다시 설정
     template_name = 'projectapp/create.html'
+
+
+class ProjectDetailView(DetailView):
+    model = Project
+    context_object_name = 'target_project'
+    template_name = 'projectapp/detail.html'
+
