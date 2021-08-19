04697cc50be69e123c661cc99380b985ed936f77
als90 <als90915@gmail.com>
2021-08-19 11:13:25 +0900

    8/19 commit_1

--- articleapp/models.py
+++ articleapp/models.py
@@ -2,12 +2,15 @@ from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from projectapp.models import Project
 
 
 class Article(models.Model):
     writer = models.ForeignKey(User, on_delete=models.SET_NULL,
                                related_name='article', null=True)
     # on_delete : 연결된 user가 삭제되었을때 경로 -> set_null: 작성자 미상
+    project = models.ForeignKey(Project, on_delete=models.SET_NULL,
+                                related_name='article', null=True)
     title = models.CharField(max_length=200, null=True)
     image = models.ImageField(upload_to='article/', null=True)
     # media 폴더의 article 폴더에 image 저장됨
