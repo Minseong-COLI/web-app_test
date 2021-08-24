6f6f419bc4b306e37804e3c7fcffa085987ca62f
als90 <als90915@gmail.com>
2021-08-24 18:08:03 +0900

    8/24 commit_5

--- likeapp/views.py
+++ likeapp/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
@@ -21,13 +22,16 @@ class LikeArticleView(RedirectView):
         like_record = LikeRecord.objects.filter(user=user, article=article)
 
         if like_record.exists():
+            # 좋아요 반영되지 않은 경우
+            messages.add_message(request, messages.ERROR, '좋아요는 한번만 가능합니다.')
             return HttpResponseRedirect(reverse('articleapp:detail',
                                                 kwargs={'pk': kwargs['article_pk']}))
         else:
             LikeRecord(user=user, article=article).save()
-
         article.like += 1
         article.save()
+        # 좋아요 반영된 후
+        messages.add_message(request, messages.SUCCESS, '좋아요를 눌렀습니다.')
 
         return super().get(self, request, *args, **kwargs)
 
