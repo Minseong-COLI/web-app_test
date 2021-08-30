a8ca25bbfb69418a44481c2b528594bbfe5b0e47
als90 <als90915@gmail.com>
2021-08-30 11:16:30 +0900

    8/30 commit_1

--- articleapp/models.py
+++ articleapp/models.py
@@ -10,7 +10,7 @@ class Article(models.Model):
                                related_name='article', null=True)
     # on_delete : 연결된 user가 삭제되었을때 경로 -> set_null: 작성자 미상
     project = models.ForeignKey(Project, on_delete=models.SET_NULL,
-                                related_name='article', null=True)
+                                related_name='article', null=True, blank=True)
     title = models.CharField(max_length=200, null=True)
     image = models.ImageField(upload_to='article/', null=True)
     # media 폴더의 article 폴더에 image 저장됨
