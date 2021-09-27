fee87d696f0ab4563f6688dc24b6f78337ae0fc9
als90 <als90915@gmail.com>
2021-09-27 11:49:45 +0900

    9/27 commit_3

--- coli_20210629/settings/deploy.py
+++ coli_20210629/settings/deploy.py
@@ -1,26 +1,34 @@
 from .base import *
 
-env_list = dict()
-
-local_env = open(os.path.join(BASE_DIR, '.env'))
-# os: 운영체제, path: 경로, join: 합치기 메서드, base_dir: settings.py의 부모+부모 경로->venv 폴더, .env 파일을 경로지정
-
-while True:
-    line = local_env.readline()
-    if not line:
-        break
-    line = line.replace('\n','')       # 줄바꿈->공백으로 치환
-    start = line.find('=')             # =의 위치를 파악해서 그 index를 start라는 변수 에 저장
-    key = line[:start]                 # start('=')를 기준으로 슬라이싱-> key, value 지정
-    value = line[start:]
-    env_list[key] = value              # env_list 딕셔너리에 key, value 값 저장
+# env_list = dict()
+#
+# local_env = open(os.path.join(BASE_DIR, '.env'))
+# # os: 운영체제, path: 경로, join: 합치기 메서드, base_dir: settings.py의 부모+부모 경로->venv 폴더, .env 파일을 경로지정
+#
+# while True:
+#     line = local_env.readline()
+#     if not line:
+#         break
+#     line = line.replace('\n','')       # 줄바꿈->공백으로 치환
+#     start = line.find('=')             # =의 위치를 파악해서 그 index를 start라는 변수 에 저장
+#     key = line[:start]                 # start('=')를 기준으로 슬라이싱-> key, value 지정
+#     value = line[start:]
+#     env_list[key] = value              # env_list 딕셔너리에 key, value 값 저장
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+def read_secret(secret_name):
+    file = open('/run/secrets/' + secret_name)
+    secret = file.read()
+    secret = secret.lstrip().rstrip()
+    file.close()
+    return secret
 
-SECRET_KEY = env_list['SECRET_KEY']
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
+
+# SECRET_KEY = env_list['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -34,8 +42,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django',
-        'USER': 'django',
-        'PASSWORD': '1234',
+        'USER': read_secret('MARIADB_USER'),
+        'PASSWORD': read_secret('MARIADB_PASSWORD'),
         'HOST': 'mariadb',
         'PORT': '3306',
         # mariadb의 기본 포트: 3306
